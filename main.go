d4ec5df782795ffa75337126cd605b7d38f9c1dd
primaaku <primaaguskuncoro@gmail.com>
2022-08-08 22:28:44 +0700

    updatemain  delete complete

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	http.HandleFunc("/", taskcontroller.Index)
 	http.HandleFunc("/task/get_form", taskcontroller.GetForm)
 	http.HandleFunc("/task/store", taskcontroller.Store)
+	http.HandleFunc("/task/delete", taskcontroller.Delete)
+	http.HandleFunc("/task/complete", taskcontroller.Complete)
 
 	http.ListenAndServe(":8000", nil)
 
