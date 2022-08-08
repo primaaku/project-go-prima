6180396151daf6949cfde34b33992df90ea4484d
primaaku <primaaguskuncoro@gmail.com>
2022-08-08 11:01:00 +0700

    commit nama db

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ func DBConnection() (*sql.DB, error) {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbPass := ""
-	dbName := "go_prima "
+	dbName := "go_prima"
 
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	return db, err
