b4ed3e38299f70f8cfec427f2e1278359bcc4118
primaaku <primaaguskuncoro@gmail.com>
2022-08-08 09:35:57 +0700

    update view html

--- controllers/taskcontroller/taskcontroller.go
+++ controllers/taskcontroller/taskcontroller.go
@@ -1,10 +1,12 @@
 package taskcontroller
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Berjalan")
+	// fmt.Println("Berjalan")
+	temp, _ := template.ParseFiles("views/task/index.html")
+	temp.Execute(w, nil)
 }
