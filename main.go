a7ff2cb2fb79667324bba0487d1bc5f406453ce5
primaaku <primaaguskuncoro@gmail.com>
2022-08-10 15:32:04 +0700

    update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/primaaku/project-go-prima/controllers/taskcontroller"
 )
@@ -14,6 +15,13 @@ func main() {
 	http.HandleFunc("/task/delete", taskcontroller.Delete)
 	http.HandleFunc("/task/complete", taskcontroller.Complete)
 
-	http.ListenAndServe(":8000", nil)
+	// http.ListenAndServe(":8000", nil)
+
+	// appPort := ":" + os.Getenv("PORT")
+	// fmt.Println(appPort) // This prints ":8000"
+	// s := &http.Server{
+	// 	Addr: ":8000",
+	// }
+	http.ListenAndServe(os.Getenv("PORT"), nil)
 
 }
